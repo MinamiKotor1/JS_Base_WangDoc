415481b4c7fa11d8b0a915362f276e3c95bfa96f
MinamiKotor1 <18842832912@mail.nwpu.edu.cn>
2024-02-06 02:26:21 +0800

    2024.2.6

--- 1. 入门/入门.js
+++ 1. 入门/入门.js
@@ -1,6 +1,12 @@
-var a = 1+2
-console.log(a)
+// var a = 1+2
+// console.log(a)
 
 // console.log(a);
 // var a = 1;
 
+let even = 4;
+let ifEven;
+
+ifEven = even % 2 === 0;
+
+console.log(ifEven);
