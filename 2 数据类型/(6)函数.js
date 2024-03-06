f1c294bc1ee4aa57d62b9cad4c53124746d5fc3f
MinamiKotor1 <18842832912@mail.nwpu.edu.cn>
2024-03-06 22:00:29 +0800

    2024.3.6

--- 2 数据类型/(6)函数.js
+++ 2 数据类型/(6)函数.js
@@ -4,8 +4,50 @@
 //
 // print('hello world');
 
-f();
+// f();
+//
+// let f = function () {
+//
+// }
 
-let f = function () {
+// var f = function () {
+//     console.log(1);
+// }
+//
+// function f() {
+//     console.log(2)
+// }
+//
+// f()
 
-}
+// let f = function () {
+//     console.log(2)
+// }
+//
+// f()
+
+// let  myFunc = function () {
+//
+// };
+//
+// function test(f) {
+//     console.log(f.name);
+// }
+//
+// // test(myFunc)
+//
+// console.log(test.length)
+
+let multiline = function (fn) {
+    let arr = fn.toString().split('\n');
+    console.log(arr)
+    console.log(arr.slice(1, arr.length - 1))
+    return arr.slice(1, arr.length - 1).join('\n');
+};
+
+function f() {/*
+  这是一个
+  多行注释
+*/}
+
+console.log(multiline(f));
